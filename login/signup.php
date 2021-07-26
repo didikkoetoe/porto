1116274289d370dafd4945e2529d5d05b1cf93d0
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-26 10:37:08 +0700

    Membuat logika registrasi user

--- login/signup.php
+++ login/signup.php
@@ -1,3 +1,32 @@
+<?php
+
+// Koneksi ke halaman functions.php
+require "../functions/functions.php";
+
+if (isset($_POST["daftar"])) {
+    // Cek apakah password dan konfirmasi password sama
+    if ($_POST["password"] != $_POST["password2"]) {
+        echo "<script>
+        alert('Password tidak sama');
+        document.location.href = 'signup.php';
+        </script>";
+        die;
+    }
+
+    if (registrasi($_POST) > 0) {
+        echo "<script>
+        alert('Data berhasil di tambahkan');
+        document.location.href = 'login.php';
+        </script>";
+    } else {
+        echo "<script>
+        alert('Data gagal di tambahkan');
+        document.location.href = 'login.php';
+        </script>";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -20,25 +49,25 @@
                     <div class="mb-3">
                         <div class="input-group">
                             <div class="input-group-text"><i class="fas fa-user"></i></div>
-                            <input type="text" name="username" class="form-control" placeholder="Masukan username" autocomplete="off">
+                            <input type="text" name="username" class="form-control" placeholder="Masukan username" autocomplete="off" required>
                         </div>
                     </div>
                     <div class="mb-3">
                         <div class="input-group">
                             <div class="input-group-text"><i class="fas fa-at"></i></div>
-                            <input type="email" name="email" class="form-control" placeholder="Masukan email">
+                            <input type="email" name="email" class="form-control" placeholder="Masukan email" required>
                         </div>
                     </div>
                     <div class="mb-3">
                         <div class="input-group">
                             <div class="input-group-text"><i class="fas fa-lock-open"></i></div>
-                            <input type="password" name="password" class="form-control" placeholder="Masukan password">
+                            <input type="password" name="password" class="form-control" placeholder="Masukan password" required>
                         </div>
                     </div>
                     <div class="mb-3">
                         <div class="input-group">
                             <div class="input-group-text"><i class="fas fa-lock"></i></div>
-                            <input type="password" name="password2" class="form-control" placeholder="Masukan kembali password">
+                            <input type="password" name="password2" class="form-control" placeholder="Masukan kembali password" required>
                         </div>
                     </div>
                     <a href="login.php" class="btn btn-danger">Kembali</a>
