c8d77e27c420fbcbfc54ad1279af8a4c94aeff3e
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-26 10:04:14 +0700

    Membuat form daftar

--- login/signup.php
+++ login/signup.php
@@ -12,7 +12,42 @@
 </head>
 
 <body>
-
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2 class="text-center">Daftar Akun</h2>
+                <form action="" class="form" method="POST">
+                    <div class="mb-3">
+                        <div class="input-group">
+                            <div class="input-group-text"><i class="fas fa-user"></i></div>
+                            <input type="text" name="username" class="form-control" placeholder="Masukan username" autocomplete="off">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="input-group">
+                            <div class="input-group-text"><i class="fas fa-at"></i></div>
+                            <input type="email" name="email" class="form-control" placeholder="Masukan email">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="input-group">
+                            <div class="input-group-text"><i class="fas fa-lock-open"></i></div>
+                            <input type="password" name="password" class="form-control" placeholder="Masukan password">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="input-group">
+                            <div class="input-group-text"><i class="fas fa-lock"></i></div>
+                            <input type="password" name="password2" class="form-control" placeholder="Masukan kembali password">
+                        </div>
+                    </div>
+                    <a href="login.php" class="btn btn-danger">Kembali</a>
+                    <button class="btn btn-secondary" type="reset">Reset</button>
+                    <button name="daftar" class="btn btn-primary">Daftar</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
