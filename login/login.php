f1d5b8d9a779b0f598d697a865bb84148994387d
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-26 08:12:09 +0700

    Membuat form login

---
+++ login/login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../fontawesome/css/all.min.css">
+    <script src="../js/jquery.js"></script>
+</head>
+
+<body>
+
+    <!-- Form login -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md">
+                <h2 class="text-center">Login</h2>
+                <form action="" class="form" method="POST">
+                    <div class="mb-3">
+                        <div class="input-group">
+                            <div class="input-group-text"><i class="fas fa-user"></i></div>
+                            <input type="text" class="form-control" placeholder="Masukan username" name="username" autocomplete="off">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="input-group">
+                            <div class="input-group-text"><i class="fas fa-lock"></i></div>
+                            <input type="password" class="form-control" placeholder="Masukan password">
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">Ingat saya</label>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary">Masuk</button>
+                    <hr>
+                    <button class="btn btn-success">Sign Up</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- Akhir form login -->
+
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
