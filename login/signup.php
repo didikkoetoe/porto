4377e0172e85bd59845f8d623fbd3fe5ac4b27a9
Didik Prabowo <danieldidikprabowo@gmail.com>
2021-07-26 13:55:29 +0700

    Menghubungkan dengan cssnya dar membuat border

--- login/signup.php
+++ login/signup.php
@@ -37,6 +37,7 @@ if (isset($_POST["daftar"])) {
     <title>Sign Up</title>
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="../css/signup.css">
     <script src="../js/jquery.js"></script>
 </head>
 
@@ -44,36 +45,40 @@ if (isset($_POST["daftar"])) {
     <div class="container">
         <div class="row">
             <div class="col">
-                <h2 class="text-center">Daftar Akun</h2>
-                <form action="" class="form" method="POST">
-                    <div class="mb-3">
-                        <div class="input-group">
-                            <div class="input-group-text"><i class="fas fa-user"></i></div>
-                            <input type="text" name="username" class="form-control" placeholder="Masukan username" autocomplete="off" required>
+                <div class="border rounded px-3">
+                    <h2 class="text-center">Daftar Akun</h2>
+                    <form action="" class="form" method="POST">
+                        <div class="mb-3">
+                            <div class="input-group">
+                                <div class="input-group-text"><i class="fas fa-user"></i></div>
+                                <input type="text" name="username" class="form-control" placeholder="Masukan username" autocomplete="off" required>
+                            </div>
                         </div>
-                    </div>
-                    <div class="mb-3">
-                        <div class="input-group">
-                            <div class="input-group-text"><i class="fas fa-at"></i></div>
-                            <input type="email" name="email" class="form-control" placeholder="Masukan email" required>
+                        <div class="mb-3">
+                            <div class="input-group">
+                                <div class="input-group-text"><i class="fas fa-at"></i></div>
+                                <input type="email" name="email" class="form-control" placeholder="Masukan email" required>
+                            </div>
                         </div>
-                    </div>
-                    <div class="mb-3">
-                        <div class="input-group">
-                            <div class="input-group-text"><i class="fas fa-lock-open"></i></div>
-                            <input type="password" name="password" class="form-control" placeholder="Masukan password" required>
+                        <div class="mb-3">
+                            <div class="input-group">
+                                <div class="input-group-text"><i class="fas fa-lock-open"></i></div>
+                                <input type="password" name="password" class="form-control" placeholder="Masukan password" required>
+                            </div>
                         </div>
-                    </div>
-                    <div class="mb-3">
-                        <div class="input-group">
-                            <div class="input-group-text"><i class="fas fa-lock"></i></div>
-                            <input type="password" name="password2" class="form-control" placeholder="Masukan kembali password" required>
+                        <div class="mb-3">
+                            <div class="input-group">
+                                <div class="input-group-text"><i class="fas fa-lock"></i></div>
+                                <input type="password" name="password2" class="form-control" placeholder="Masukan kembali password" required>
+                            </div>
                         </div>
-                    </div>
-                    <a href="login.php" class="btn btn-danger">Kembali</a>
-                    <button class="btn btn-secondary" type="reset">Reset</button>
-                    <button name="daftar" class="btn btn-primary">Daftar</button>
-                </form>
+                        <div class="mb-3">
+                            <a href="login.php" class="btn btn-danger">Kembali</a>
+                            <button class="btn btn-secondary" type="reset">Reset</button>
+                            <button name="daftar" class="btn btn-primary">Daftar</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
